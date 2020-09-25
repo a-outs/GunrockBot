371075d3fde7c91447b985cb3bc763c29f57ee6d
joshjun010202@gmail.com <joshjun010202@gmail.com>
2020-09-26 00:53:25 +0900

    Changed import

--- Gunrock.py
+++ Gunrock.py
@@ -1,5 +1,5 @@
 import discord
-#from discord.ext import menus
+from discord.ext import menus
 from discord.ext import commands
 #from discord.ext.commands import Bot
 from discord.ext.commands import has_permissions
